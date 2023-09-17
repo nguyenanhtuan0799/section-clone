742e777df82afc6bef7247c755848377985ba252
tuannguyen <nguyennanhtuan0799@gmail.com>
2023-09-17 20:03:53 +0700

    done

---
+++ src/mockData/products.ts
@@ -0,0 +1,19 @@
+export const PRODUCTS_DATA = [
+  {
+    label: "TOI 3D Customize E-commerce",
+    img: "https://tekup.vn/wp-content/uploads/2022/09/image-1.png",
+  },
+  {
+    label: "E-learning – Internal training platform",
+    img: "https://tekup.vn/wp-content/uploads/2022/09/image.png",
+  },
+  {
+    label: "Summer 21 Cosmetic E-commerce Platform",
+    img: "https://tekup.vn/wp-content/uploads/2022/08/summer21-thumbnail.png",
+  },
+  {
+    label:
+      "Kiva – Ambition to digital transformation in the brokerage assiduity",
+    img: "https://tekup.vn/wp-content/uploads/2022/08/kiva-thumbnail.png",
+  },
+];
